acf481e8c9bf7d5d48e24e980d84d508a54fb8fa
ayleenrq <qisthyayleen@gmail.com>
2026-05-02 19:17:27 +0700

    add categories section

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,8 +1,10 @@
 import Image from "next/image";
 import HeroSection from "./components/home/hero";
+import CategoriesSection from "./components/home/categories";
 
 export default function Home() {
   return <main>
     <HeroSection />
+    <CategoriesSection />
   </main>
 }
